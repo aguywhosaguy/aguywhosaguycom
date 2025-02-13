86e54b3e31b23fac413620a5d237282b11c05136
aguywhosaguy <caseypeg8@gmail.com>
2025-02-12 21:39:02 -0600

    fix: fixed link for vercel

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,6 +1,17 @@
+import 'dotenv/config'
 import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
 import type { AppRouter } from "./routers/_app";
 
+const getRootURL = () => {
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}/api/trpc`;
+  return "http://localhost:3000/api/trpc";
+}
+
 export const client = createTRPCClient<AppRouter>({
-  links: [loggerLink(), httpBatchLink({ url: "http://localhost:3000/api/trpc" })]
+  links: [
+    loggerLink(),
+    httpBatchLink({ 
+      url: getRootURL()
+    })
+  ]
 });
